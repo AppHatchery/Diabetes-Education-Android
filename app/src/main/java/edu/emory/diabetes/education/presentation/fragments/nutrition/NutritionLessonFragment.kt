ab7271f6d8b807718cd1c47589883e343d0bfd68
evance-mose <evasmose@gmail.com>
2022-10-10 11:36:02 +0200

    Nutrition lesson Bug fixing

--- app/src/main/java/edu/emory/diabetes/education/presentation/fragments/nutrition/NutritionLessonFragment.kt
+++ app/src/main/java/edu/emory/diabetes/education/presentation/fragments/nutrition/NutritionLessonFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import edu.emory.diabetes.education.R
 import edu.emory.diabetes.education.databinding.FragmentDiabetesNutritionLessonBinding
 import edu.emory.diabetes.education.presentation.BaseFragment
-import edu.emory.diabetes.education.presentation.fragments.basic.BasicNavigator
 
 
 class NutritionLessonFragment(
@@ -19,7 +18,7 @@ class NutritionLessonFragment(
         with(FragmentDiabetesNutritionLessonBinding.bind(view)) {
             navigation = fragment as NutritionNavigator
             adapter = NutritionLessonAdapter {
-                navigation.invoke(it)
+                navigation.invoke(lesson = it, event = Event.Lesson)
 
             }.apply {
                 submitList(NutritionUtils.lessonData.map {
