20c61ea7f88f321380ed799068932ffb907b01d7
Comfort Mwalija <calmwalija@gmail.com>
2023-04-27 05:56:16 +0200

    Add support email to about page

--- app/src/main/java/edu/emory/diabetes/education/presentation/fragments/aboutus/AboutAsContentFragment.kt
+++ app/src/main/java/edu/emory/diabetes/education/presentation/fragments/aboutus/AboutAsContentFragment.kt
@@ -3,10 +3,8 @@ package edu.emory.diabetes.education.presentation.fragments.aboutus
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.text.method.LinkMovementMethod
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import edu.emory.diabetes.education.Ext
 import edu.emory.diabetes.education.R
 import edu.emory.diabetes.education.databinding.FragmentAboutUsContentBinding
@@ -25,6 +23,7 @@ class AboutAsContentFragment : BaseFragment(R.layout.fragment_about_us__content)
                 startActivity(chooser)
             }
         }
+            sendMail.movementMethod = LinkMovementMethod.getInstance()
 
        }
     }
