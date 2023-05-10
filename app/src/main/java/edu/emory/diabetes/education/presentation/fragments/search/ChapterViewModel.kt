1579ed777ee57397e2db007d688110c986e80cd3
kennedy <kennedylinzie@gmail.com>
2023-05-10 11:12:13 +0200

    8 improvements to search iteration (#94)
    * Tapping on search result should scroll the html to the position where that line is found
    
    * Update WhatIsDiabetes.kt
    
    * Update WhatIsDiabetes.kt
    
    * unifying the search functionality and table searches implementation
    
    There has been a creation of a new file SearchUtils.kt this class has string modification functions and a class responsible for searching for matches ,this is because the WhatIsDiabetes .kt and ResourceWebViewFragment.kt has search implementation done on the particular classes that means full implementation on each so, this is a refactor to separate the search functionality.
    
    * Update SearchUtils.kt
    
    * delocalization of search and highlighting
    
    * Update WhatIsDiabetes.kt
    
    * search changes
    
    * implementing highting and scroll
    
    * Update WhatIsDiabetes.kt
    
    * cleaning after merge
    
    * Restructuring
    
    the scroll Views have been removed, they prevent the web view from scrolling inside and the scroll percentage indicator has been replace with the web vies scrolling is now smooth and targets the item appropriatly
    
    * clear matches
    
    * Update SearchUtils.kt
    
    * Update SearchUtils.kt
    
    * clean
    
    * touch the search result scroll to it
    
    * Update ChapterViewModel.kt
    
    * Update ChapterViewModel.kt
    
    * 75% string trimming
    
    * duplicate controls in nutritions
    
    * trimming
    
    * improvements
    
    * Update SearchUtils.kt
    
    * another version
    
    * improvement
    
    * Update fragment_search_chapter.xml
    
    * remove word scrolling
    
    * Update WhatIsDiabetes.kt

--- app/src/main/java/edu/emory/diabetes/education/presentation/fragments/search/ChapterViewModel.kt
+++ app/src/main/java/edu/emory/diabetes/education/presentation/fragments/search/ChapterViewModel.kt
@@ -1,5 +1,9 @@
 package edu.emory.diabetes.education.presentation.fragments.search
 
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.emory.diabetes.education.views.WebAppInterface
@@ -20,6 +24,7 @@ class ChapterViewModel @Inject constructor(
     val searchResult = searchFlow
     fun search(searchQuery: String): Flow<List<String>> {
         return flow {
+
             val res = mutableListOf<String>()
             WebAppInterface.parsedData.split( "?", ":","_").forEach {
                 if (it.contains("∧")){
@@ -28,7 +33,13 @@ class ChapterViewModel @Inject constructor(
                     if (it.contains(searchQuery,ignoreCase = true)) res.add(it)
                 }
             }
-            emit(res)
+            val colorTag ="<span style='background-color: yellow; color: black; font-weight: bold;'>${searchQuery}</span>"
+            for (i in res.indices) {
+                if (res[i].contains(searchQuery,ignoreCase = true)) {
+                    res[i] = res[i].replace(searchQuery, colorTag,ignoreCase = true)
+                }
+            }
+            emit(res.distinct())
         }
     }
 
