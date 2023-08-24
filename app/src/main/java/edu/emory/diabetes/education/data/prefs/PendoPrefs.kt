1d1f7c55f194da048d637683db1c3f6050e814d3
Comfort Mwalija <42068747+calmwalija@users.noreply.github.com>
2023-08-24 19:15:51 +0200

    Set up Pendo Identifier to track visitors (#123)

---
+++ app/src/main/java/edu/emory/diabetes/education/data/prefs/PendoPrefs.kt
@@ -0,0 +1,34 @@
+package edu.emory.diabetes.education.data.prefs
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class PendoPrefs @Inject constructor(
+  @ApplicationContext context: Context
+) {
+
+
+  private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "net.techandgraphics.hymn.data.prefs")
+  private val dataStore = context.dataStore
+
+  companion object {
+
+    const val PENDO_PREFIX = "Sep-23-"
+    private val pendoVisitorId = stringPreferencesKey("prefs_pendo_visitor_id")
+
+  }
+
+  suspend fun setPendoVisitorId(id: String) = dataStore.edit { it[pendoVisitorId] = id }
+  val getPendoVisitorId = dataStore.data.map { it[pendoVisitorId] }
+
+
+}
