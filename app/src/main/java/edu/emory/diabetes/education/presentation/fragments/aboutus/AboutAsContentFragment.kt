3847bed0f58e4425844857b49c4e9a593a34b13b
Comfort Mwalija <42068747+calmwalija@users.noreply.github.com>
2023-09-08 05:50:47 +0200

    Add Privacy policy to TypeU Google (#126)

--- app/src/main/java/edu/emory/diabetes/education/presentation/fragments/aboutus/AboutAsContentFragment.kt
+++ app/src/main/java/edu/emory/diabetes/education/presentation/fragments/aboutus/AboutAsContentFragment.kt
@@ -2,14 +2,18 @@ package edu.emory.diabetes.education.presentation.fragments.aboutus
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.View
+import edu.emory.diabetes.education.BuildConfig
 import edu.emory.diabetes.education.Ext
 import edu.emory.diabetes.education.R
 import edu.emory.diabetes.education.databinding.FragmentAboutUsContentBinding
 import edu.emory.diabetes.education.presentation.BaseFragment
 
+private const val PRIVACY_POLICY_URL =
+    "https://www.choa.org/-/media/Files/Childrens/patients/patient-privacy-notice-2021.pdf"
 
 class AboutAsContentFragment : BaseFragment(R.layout.fragment_about_us__content){
 
@@ -23,6 +27,15 @@ class AboutAsContentFragment : BaseFragment(R.layout.fragment_about_us__content)
                 startActivity(chooser)
             }
         }
+
+            privacyPolicy.setOnClickListener {
+                requireActivity().startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse(PRIVACY_POLICY_URL))
+                )
+            }
+
+            // Set the version name in the build.gradle file
+            version.text = String.format("v%s", BuildConfig.VERSION_NAME)
 
             /*Attach click event on the anchor tags in the html string resource*/
             sendMail.movementMethod = LinkMovementMethod.getInstance()
