ab42c60a859ff95a8de32aa2261eea6de29105aa
Naomi Nyama <nyamanaomi38@gmail.com>
2026-06-01 08:22:03 +0200

    version code and Pendo back to Test account

--- app/src/main/java/edu/emory/diabetes/education/App.kt
+++ app/src/main/java/edu/emory/diabetes/education/App.kt
@@ -21,7 +21,7 @@ class App : Application() {
   lateinit var pendoPrefs: PendoPrefs
 
   companion object {
-    private const val PENDO_ACCOUNT_ID = "TypeU-Pilot"
+    private const val PENDO_ACCOUNT_ID = "TypeU-Test"
   }
 
   override fun onCreate() {
